aea00a7d9e2adabcacea187db555b9aec94858d4
Gopi Kalyan <gopi.kalyan65@gmail.com>
2012-09-03 18:13:16 +0530

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,10 +11,9 @@ describe "Static pages" do
 
 	describe "Home page" do
     before { visit root_path }
-    let(:heading)    { 'Microblogger' }
-    let(:page_title) { '' }
 
-    it_should_behave_like "all static pages"
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
 
 		describe "for signed-in users" do
@@ -30,6 +29,17 @@ describe "Static pages" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+			describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
